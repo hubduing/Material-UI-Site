b0fb977968e014676568fd929aa3ccc2d0c7c737
Alexey Borisov <UralMana@yandex.ru>
2024-11-09 13:40:40 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import styles from "./App.module.scss";
+import Box from "@mui/material/Box";
+import MenuIcon from "@mui/icons-material/Menu";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              className={styles.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={styles.title}>
+              Material UI-Site
+            </Typography>
+            <div className={styles.grow} />{" "}
+            {/* Этот div отталкивает кнопки вправо */}
+            <Box mr={3}>
+              <Button color="inherit" variant="outlined">
+                Log In
+              </Button>
+            </Box>
+            <Button color="secondary" variant="contained">
+              Sign Up
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </div>
   );
 }
-
 export default App;
