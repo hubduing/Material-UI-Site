5fd733b9d7e25b567682b134db30a0c9f4afc83b
Alexey Borisov <UralMana@yandex.ru>
2024-11-09 16:29:52 +0500

    Fix/ Main import App

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,49 @@
 import React from "react";
-import { Container, Grid, Paper, Button, Typography } from "@material-ui/core";
-import styles from "./Main.module.scss";
-const Main = () => {
+import { Button, Container, Grid, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  mainFeaturesPost: {
+    position: "relative",
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  },
+  mainFeaturesPostContent: {
+    position: "relative",
+    padding: theme.spacing(9),
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0, .3)",
+  },
+}));
+function Main() {
+  const classes = useStyles();
   return (
     <main>
-      <Paper style={styles.mainFeaturesPost}>
-        <Container maxWidth="md">
+      <Paper
+        className={classes.mainFeaturesPost}
+        style={{
+          backgroundImage: `url(https://images.unsplash.com/photo-1730407401172-aeed1b1ace5b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+        }}
+      >
+        <Container maxWidth="lg">
+          <div className={classes.overlay} />
           <Grid>
             <Grid item md={6}>
-              <div style={styles.mainFeaturesPostContent}>
-                <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              <div className={classes.mainFeaturesPostContent}>
+                <Typography
+                  component="h1"
+                  variant="h3"
+                  color="inherit"
+                  gutterBottom
+                >
                   Material UI Site
                 </Typography>
                 <Typography variant="h5" color="inherit" paragraph>
@@ -28,5 +62,5 @@ const Main = () => {
       </Paper>
     </main>
   );
-};
+}
 export default Main;
